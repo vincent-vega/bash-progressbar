377efe809d18d89ad35bf4b49e800ce8a13d615d
dart <bar.davide@gmail.com>
2017-02-25 18:20:24 +0100

    test function added

--- progressbar.sh
+++ progressbar.sh
@@ -4,26 +4,27 @@ function progressbar() {
     if [ -z $1 ] || [ -z $2 ] || [ "$1" -gt "$2" ] ; then
         return
     fi
-    performed=$1
-    total=$2
+    local performed=$1
+    local total=$2
     if [ -z $3 ]; then
-        bar_length=23 # wget size
+        local bar_length=23 # wget size
     else
-        bar_length=$(($3-1)) # considering '>' character
+        local bar_length=$(($3-1)) # considering '>' character
     fi
 
-    count=`echo $performed $total $bar_length | awk '{ print int(($1*$3)/$2) }'`
-    perc=`echo $performed $total | awk '{ print int($1*100/$2) }'`
+    local count=`echo $performed $total $bar_length | awk '{ print int(($1*$3)/$2) }'`
+    local perc=`echo $performed $total | awk '{ print int($1*100/$2) }'`
 
     echo -n $perc%\[
-    for i in `seq 1 $bar_length`; do
+    for i in `seq 1 $(($bar_length+1))`; do
     if (("$i" <= "$count")); then
         echo -n =
     else
         if (("$i" == "$(($count+1))")); then
             echo -n '>'
+        else
+            echo -n ' '
         fi
-        echo -n ' '
     fi
     done
     echo \]
@@ -31,5 +32,12 @@ function progressbar() {
 
 function percentagebar() {
     progressbar $1 100 $2
+}
+
+function testbar() {
+    for i in {1..10}; do
+        echo -ne "`percentagebar $(($i*10))`\033[0K\r"
+    done
+    echo
 }
 
